e083f024effd24cf6f93fcce40feccef69d22466
Örs Csiszer <csiszerors@gmail.com>
2024-02-18 19:30:40 +0200

    spring init

---
+++ eBloc/src/main/java/bigFour/eBloc/EBlocApplication.java
@@ -0,0 +1,13 @@
+package bigFour.eBloc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EBlocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EBlocApplication.class, args);
+	}
+
+}
---
+++ eBloc/src/test/java/bigFour/eBloc/EBlocApplicationTests.java
@@ -0,0 +1,13 @@
+package bigFour.eBloc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EBlocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
